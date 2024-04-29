6b9d52057425710ff1c30d439021e3d7f452d448
emery97 <S10258645@connect.np.edu.sg>
2024-04-30 00:55:20 +0800

    doesnt workk TT tmr try agn lol

--- app/src/main/java/sg/edu/np/mad/madpractical3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical3/MainActivity.java
@@ -15,7 +15,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 
 public class MainActivity extends AppCompatActivity {
-
+//
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
